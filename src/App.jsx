145cec2fa38c378a54fb6b7ee7c6d60d40c1a7b5
sinaTejeh <amrjv1378@gmail.com>
2026-02-23 20:51:05 +0330

    creat newTask component and creat Input component for stop repetitive code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewTask from "./components/NewTask";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Sidebar />
+      <NewTask />
     </main>
   );
 }
