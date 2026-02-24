6f49acf861d3548b659fb367ad758c31816558f0
sinaTejeh <amrjv1378@gmail.com>
2026-02-24 11:17:45 +0330

    add NoTask component to App.jsx file instead of NewTask component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewTask from "./components/NewTask";
+import NoTaskSelected from "./components/NoTaskSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
-      <NewTask />
+      <NoTaskSelected />
     </main>
   );
 }
