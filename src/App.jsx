d303bf1c206d408e5b290f052c24edd0ec0d244a
sinaTejeh <amrjv1378@gmail.com>
2026-02-24 13:37:48 +0330

    use handleStartTask for buttons in sidebar and noTask components to show the NewTask component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
+
 import NewTask from "./components/NewTask";
 import NoTaskSelected from "./components/NoTaskSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [tasksState, setTasksState] = useState({
+    selectedTaskId: undefined,
+    tasks: []
+  });
+
+  const handleStartTask = function () {
+    setTasksState(prevTasks => {
+      return {
+        ...prevTasks,
+        selectedTaskId: null,
+      }
+    })
+  };
+
+  let content;
+  if (tasksState.selectedTaskId === null) {
+    content = <NewTask />
+  } else if (tasksState.selectedTaskId === undefined) {
+    content = <NoTaskSelected onStartAddTask={handleStartTask} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoTaskSelected />
+      <Sidebar onStartAddTask={handleStartTask} />
+      {content}
     </main>
   );
 }
--- src/components/NoTaskSelected.jsx
+++ src/components/NoTaskSelected.jsx
@@ -1,13 +1,13 @@
 import noTasks from '../assets/no-Tasks.png';
 import Button from './Button.jsx';
-export default function NoTaskSelected() {
+export default function NoTaskSelected({ onStartAddTask }) {
     return (
         <div className="mt-24 text-center w-2/3">
             <img src={noTasks} alt='An empty task list' className='w-16 h-16 object-contain mx-auto' />
             <h2 className='text-xl font-bold text-stone-500 my-4'>No Task Selected</h2>
             <p className='text-stone-400 mb-4'>Select a Task or get started a new one</p>
             <p className='mt-8'>
-                <Button title='Creat new Task' />
+                <Button title='Creat new Task' onClick={onStartAddTask} />
             </p>
 
         </div >
